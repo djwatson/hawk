95ab4164ef57d869eed9c465e6d396fc179f1a41
Dave Watson <dade.watson@gmail.com>
2023-05-09 08:46:01 -0700

    Full working tailcall

--- vm.cpp
+++ vm.cpp
@@ -44,24 +44,44 @@ __attribute__((noinline)) void EXPAND_STACK_SLOWPATH() {
 
 unsigned char hotmap[hotmap_sz];
 
+
 #define PARAMS unsigned char ra, unsigned instr,unsigned* pc, long* frame, void** op_table_arg
 #define ARGS ra, instr, pc, frame, op_table_arg
 #define MUSTTAIL __attribute__((musttail))
 //#define DEBUG(name) printf("%s %li %li %li %li\n", name, frame[0], frame[1], frame[2], frame[3]);
 #define DEBUG(name)
-
 typedef void (*op_func)(PARAMS);
-static op_func op_table[25];
-#define NEXT_INSTR { instr = *pc;		\
-  unsigned char op = instr & 0xff;		\
-  ra = (instr >> 8) & 0xff;			\
-  instr >>= 16;					\
-  op_func* op_table_arg_c = (op_func*)op_table_arg; \
-  MUSTTAIL return op_table_arg_c[op](ARGS);		\
+static op_func l_op_table[25];
+static op_func l_op_table_record[25];
+
+#define NEXT_INSTR { instr = *pc;			\
+    unsigned char op = instr & 0xff;			\
+    ra = (instr >> 8) & 0xff;				\
+    instr >>= 16;					\
+    op_func* op_table_arg_c = (op_func*)op_table_arg;	\
+    MUSTTAIL return op_table_arg_c[op](ARGS);		\
+  }
+
+void RECORD_START(PARAMS) {
+    hotmap[(((long)pc) >> 2) & hotmap_mask] = hotmap_cnt;
+    if (joff) {
+      // Tail call with original op table.
+      MUSTTAIL return l_op_table[INS_OP(*pc)](ARGS);
+    }
+    // Tail call with recording op table, but first instruction is not recorded.
+    MUSTTAIL return l_op_table[INS_OP(*pc)](ra, instr, pc, frame, (void**)l_op_table_record);
 }
 
+void RECORD(PARAMS) {
+    if (record(pc, frame)) {
+      // Back to interpreting.
+      op_table_arg = (void**)l_op_table;
+    }
+    // Call interpret op table, but with record table.
+    // Interprets *this* instruction, then advances to next 
+    MUSTTAIL return l_op_table[INS_OP(*pc)](ra, instr, pc, frame, op_table_arg);
+}
 
-extern "C" {
 void INS_FUNC(PARAMS) {
   DEBUG("FUNC");
   pc++;
@@ -238,9 +258,7 @@ void INS_CALL(PARAMS) {
 
   if (unlikely((hotmap[(((long)pc) >> 2) & hotmap_mask] -=
 		hotmap_tail_rec) == 0)) {
-    //FAIL_SLOWPATH(0, 0);
-    hotmap[(((long)pc) >> 2) & hotmap_mask] = hotmap_cnt;
-    //goto L_INS_RECORD_START;
+    MUSTTAIL return RECORD_START(ARGS);
   }
   auto v = frame[ra + 1];
   if (unlikely((v & 0x7) != 5)) {
@@ -266,9 +284,7 @@ void INS_CALLT(PARAMS) {
 
   if (unlikely((hotmap[(((long)pc) >> 2) & hotmap_mask] -=
 		hotmap_tail_rec) == 0)) {
-    //FAIL_SLOWPATH(0, 0);
-    hotmap[(((long)pc) >> 2) & hotmap_mask] = hotmap_cnt;
-    //goto L_INS_RECORD_START;
+    MUSTTAIL return RECORD_START(ARGS);
   }
   auto v = frame[ra];
   if (unlikely((v & 0x7) != 5)) {
@@ -397,19 +413,31 @@ void INS_ISF(PARAMS) {
   NEXT_INSTR;
 }
 
+void INS_JFUNC(PARAMS) {
+  auto tnum = instr;
+  // printf("JFUNC/JLOOP run %i\n", tnum);
+  // printf("frame before %i %li %li \n", frame-stack, frame[0], frame[1]);
+  auto res = jit_run(tnum, &pc, &frame, frame_top);
+  frame_top = stack + stacksz;
+  //printf("frame after %i %li %li \n", frame-stack, frame[0], frame[1]);
+  if (unlikely(res)) {
+    // Turn on recording again
+    op_table_arg = (void**)l_op_table_record;
+  }
+  NEXT_INSTR;
+}
+
 void INS_UNKNOWN(PARAMS) {
   printf("UNIMPLEMENTED INSTRUCTION %s\n", ins_names[INS_OP(*pc)]);
   exit(-1);
 }
-}
+
 void run() {
-
-  op_table[1] = INS_KSHORT;
-
-  
+  // Bytecode stub to get us to HALT.
   unsigned int final_code[] = {CODE(CALL, 0, 1, 0), CODE(HALT, 0, 0, 0)};
   unsigned int *code = &funcs[0]->code[0];
 
+  // Initial stack setup has a return to bytecode stub above.
   stack[0] = (unsigned long)&final_code[1]; // return pc
   stack[1] = ((unsigned long)funcs[0]) + 5; // func
   frame = &stack[2];
@@ -421,85 +449,46 @@ void run() {
     hotmap[i] = hotmap_cnt;
   }
 
-  void *l_op_table[25];
-  // clang-format off
-  void* l_op_table_interpret[] = {
-  };
-  void* l_op_table_record[25];
-  // clang-format on
-  memcpy(l_op_table, l_op_table_interpret, sizeof(l_op_table));
-
-  //////////NEW:
+  // Setup instruction table.
   for(int i = 0; i < 25; i++) {
-    op_table[i] = INS_UNKNOWN;
+    l_op_table[i] = INS_UNKNOWN;
   }
-  op_table[0] = INS_FUNC;
-  op_table[1] = INS_KSHORT;
-  op_table[2] = INS_ISGE;
-  op_table[3] = INS_JMP;
-  op_table[4] = INS_RET1;
-  op_table[5] = INS_SUBVN;
-  op_table[6] = INS_CALL;
-  op_table[7] = INS_ADDVV;
-  op_table[8] = INS_HALT;
-  op_table[10] = INS_ISLT;
-  op_table[11] = INS_ISF;
-  op_table[12] = INS_SUBVV;
-  op_table[13] = INS_GGET;
-  op_table[14] = INS_GSET;
-  op_table[15] = INS_KFUNC;
-  op_table[16] = INS_CALLT;
-  op_table[17] = INS_KONST;
-  op_table[18] = INS_MOV;
-  op_table[19] = INS_ISEQ;
-  op_table[20] = INS_ADDVN;
-  op_table[21] = INS_JISEQ;
-  op_table[22] = INS_JISLT;
+  l_op_table[0] = INS_FUNC;
+  l_op_table[1] = INS_KSHORT;
+  l_op_table[2] = INS_ISGE;
+  l_op_table[3] = INS_JMP;
+  l_op_table[4] = INS_RET1;
+  l_op_table[5] = INS_SUBVN;
+  l_op_table[6] = INS_CALL;
+  l_op_table[7] = INS_ADDVV;
+  l_op_table[8] = INS_HALT;
+  l_op_table[10] = INS_ISLT;
+  l_op_table[11] = INS_ISF;
+  l_op_table[12] = INS_SUBVV;
+  l_op_table[13] = INS_GGET;
+  l_op_table[14] = INS_GSET;
+  l_op_table[15] = INS_KFUNC;
+  l_op_table[16] = INS_CALLT;
+  l_op_table[17] = INS_KONST;
+  l_op_table[18] = INS_MOV;
+  l_op_table[19] = INS_ISEQ;
+  l_op_table[20] = INS_ADDVN;
+  l_op_table[21] = INS_JISEQ;
+  l_op_table[22] = INS_JISLT;
+  l_op_table[23] = INS_JFUNC;
+  l_op_table[24] = INS_JFUNC; // JLOOP
+  for(int i = 0; i < 25; i++) {
+    l_op_table_record[i] = RECORD;
+  }
 
-    unsigned int instr = *pc;
-    unsigned char op = instr & 0xff;
-    unsigned char ra = (instr >> 8) & 0xff;
-    instr >>= 16;
-    auto op_table_arg = (void**)op_table; 
-    op_table[op](ARGS);
-    
-    free(stack);
+  // Initial tailcalling-interpreter variable setup.
+  unsigned int instr = *pc;
+  unsigned char op = instr & 0xff;
+  unsigned char ra = (instr >> 8) & 0xff;
+  instr >>= 16;
+  auto op_table_arg = (void**)l_op_table; 
+  l_op_table[op](ARGS);
 
-    
-    // case 23: {
-    // L_INS_JFUNC:
-    //   // auto tnum = INS_B(i);
-    //   // // printf("JFUNC/JLOOP run %i\n", tnum);
-    //   // // printf("frame before %i %li %li \n", frame-stack, frame[0], frame[1]);
-    //   // auto res = jit_run(tnum, &pc, &frame, frame_top);
-    //   // frame_top = stack + stacksz;
-    //   // //printf("frame after %i %li %li \n", frame-stack, frame[0], frame[1]);
-    //   // if (unlikely(res)) {
-    //   //   memcpy(l_op_table, l_op_table_record, sizeof(l_op_table));
-    //   // }
-    //   DIRECT;
-    //   break;
-    // }
-
-
-    // {
-    // L_INS_RECORD_START:
-    //   hotmap[(((long)pc) >> 2) & hotmap_mask] = hotmap_cnt;
-    //   if (joff) {
-    //     //goto *l_op_table_interpret[INS_OP(i)];
-    //   }
-    //   // memcpy(l_op_table, l_op_table_record, sizeof(l_op_table));
-    //   // // Don't record first inst.
-    //   // goto *l_op_table_interpret[INS_OP(i)];
-    // }
-
-    // {
-    // L_INS_RECORD:
-    //   // if (record(pc, frame)) {
-    //   //   memcpy(l_op_table, l_op_table_interpret, sizeof(l_op_table));
-    //   // }
-    //   // i = *pc; // recorder may have patched instruction.
-    //   //goto *l_op_table_interpret[INS_OP(i)];
-    //   1;
-    // }
+  // And after the call returns, we're done.  only HALT returns.
+  free(stack);
 }
