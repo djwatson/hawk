edbce42f4f6df0950d5e8ea346b7fe40c16b5da1
Dave Watson <dade.watson@gmail.com>
2023-05-25 16:16:36 -0700

    Working bigint! but division sucks.

--- bigint.cpp
+++ bigint.cpp
@@ -3,6 +3,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <assert.h>
+#include <gc/gc.h>
 
 struct bignum {
   long type;
@@ -13,7 +14,7 @@ struct bignum {
 };
 
 static bignum* bignum_alloc(size_t len) {
-  auto res = (bignum*)malloc(sizeof(bignum) + len*sizeof(long));
+  auto res = (bignum*)GC_malloc(sizeof(bignum) + len*sizeof(long));
   res->type = 0;
   res->len = len;
   res->used = 0;
@@ -105,10 +106,28 @@ void bignum_simplify(bignum* a) {
   }
 }
 
+int bignum_cmp(bignum* a, bignum* b) {
+  if (a->used > b->used) {
+    return 1;
+  } else if (b->used > a->used) {
+    return -1;
+  } else {
+    for(int64_t i = a->used-1; i >=0; i--) {
+      if (a->l[i] > b->l[i]) {
+	return 1;
+      } else if (b->l[i] > a->l[i]) {
+	return -1;
+      }
+    }
+  }
+  
+  return 0;
+}
+
 bignum* bignum_sub_unsigned(bignum* a, bignum* b) {
   // a *MUST* be bigger.
   assert(a->used >= b->used);
-  //assert(bigint_cmp(a, b) == 1);
+  assert(bignum_cmp(b, a) != 1);
   auto nlen = a->used;
   int borrow = 0;
   auto res = bignum_copy(a, nlen);
@@ -232,24 +251,6 @@ static void bignum_destructive_shift_right(bignum* a, uint64_t bits) {
   bignum_simplify(a);
 }
 
-int bignum_cmp(bignum* a, bignum* b) {
-  if (a->used > b->used) {
-    return 1;
-  } else if (b->used > a->used) {
-    return -1;
-  } else {
-    for(int64_t i = a->used-1; i >=0; i--) {
-      if (a->l[i] > b->l[i]) {
-	return 1;
-      } else if (b->l[i] > a->l[i]) {
-	return -1;
-      }
-    }
-  }
-  
-  return 0;
-}
-
 bool bignum_is_zero(bignum* a) {
   return a->used == 1 && a->l[0] == 0;
 }
@@ -265,6 +266,11 @@ static void bignum_destructive_or(bignum* res, bignum* o) {
 bignum* bignum_div(bignum* a, bignum* b) {
   bignum* c = bignum_alloc(a->used + 1);
   bignum_set_from_fixnum(c, 1);
+  if(bignum_cmp(a, b) != 1) {
+    bignum_set_from_fixnum(c, 0);
+    return c;
+  }
+  assert(!bignum_is_zero(b));
 
   bignum* denom = bignum_copy(b, a->used + 1);
   bignum* num = bignum_copy(a, a->used + 1);
@@ -292,48 +298,131 @@ bignum* bignum_div(bignum* a, bignum* b) {
 
 #include <gmp.h>
 
-int main() {
-  uint64_t inputs[512];
-  memset(inputs, 0, sizeof(inputs));
+// int main() {
+//   uint64_t inputs[512];
+//   memset(inputs, 0, sizeof(inputs));
 
-  size_t sz = fread(inputs, 1, 512 * sizeof(uint64_t), stdin);
-  sz /= sizeof(uint64_t);
+//   size_t sz = fread(inputs, 1, 512 * sizeof(uint64_t), stdin);
+//   sz /= sizeof(uint64_t);
   
-  auto a = bignum_alloc(2);
-  auto d = bignum_alloc(1);
-  bignum_set_from_fixnum(a, 2);
-  bignum_set_from_fixnum(d, 1);
+//   auto a = bignum_alloc(2);
+//   auto d = bignum_alloc(1);
+//   bignum_set_from_fixnum(a, 2);
+//   bignum_set_from_fixnum(d, 1);
 
-  mpz_t am;
-  mpz_t bm;
-  mpz_t dm;
+//   mpz_t am;
+//   mpz_t bm;
+//   mpz_t dm;
   
-  mpz_inits(am, bm, dm, nullptr);
-  mpz_set_ui(am, 2);
-  mpz_set_ui(dm, 1);
+//   mpz_inits(am, bm, dm, nullptr);
+//   mpz_set_ui(am, 2);
+//   mpz_set_ui(dm, 1);
   
-  for(uint64_t i =0; i < sz; i++) {
-    auto in = inputs[i];
-    if (in < 2) {
-      continue;
-    }
-    auto b = bignum_alloc(1);
+//   for(uint64_t i =0; i < sz; i++) {
+//     auto in = inputs[i];
+//     if (in < 2) {
+//       continue;
+//     }
+//     auto b = bignum_alloc(1);
     
-    bignum_set_from_fixnum(b, in);
-    mpz_set_ui(bm, in);
+//     bignum_set_from_fixnum(b, in);
+//     mpz_set_ui(bm, in);
 
-    bignum*res;
-    res = bignum_mul_unsigned(a, b);
-    mpz_mul(am, am, bm);
-    mpz_add(dm, dm, bm);
-    free(a);
-    a = res;
+//     bignum*res;
+//     res = bignum_mul_unsigned(a, b);
+//     mpz_mul(am, am, bm);
+//     mpz_add(dm, dm, bm);
+//     free(a);
+//     a = res;
 
-    res = bignum_add_unsigned(d, b);
-    free(b);
-    free(d);
-    d = res;
+//     res = bignum_add_unsigned(d, b);
+//     free(b);
+//     free(d);
+//     d = res;
+//   }
+//   {
+//     char buf1[1024];
+//     bignum_print_hex(buf1, 1024, a);
+//     printf("A: %s\n", buf1);
+//   }
+//   {
+//     char buf1[1024];
+//     bignum_print_hex(buf1, 1024, d);
+//     printf("D: %s\n", buf1);
+//   }
+//   assert(bignum_cmp(a, d) == 1);
+//   a = bignum_div(a, d);
+//   char buf1[1024];
+//   bignum_print_hex(buf1, 1024, a);
+//   printf("%s\n", buf1);
+
+//   free(a);
+
+//   char buf2[1024];
+//   gmp_printf("A: %Zx\n", am);
+//   gmp_printf("D: %Zx\n", dm);
+//   mpz_tdiv_q(am, am, dm);
+//   gmp_snprintf(buf2, 1024, "%Zx", am);
+//   printf("%s\n", buf2);
+//   assert(strcmp(buf2, buf1) == 0);
+
+//   mpz_clears(am, bm, dm, nullptr);
+//   return 0;
+// }
+
+bignum* expt(bignum* num, uint64_t exp) {
+  auto res = bignum_copy(num, num->used);
+  for(uint64_t i = 0; i < exp - 1; i++) {
+    res = bignum_mul_unsigned(res, num);
   }
+  return res;
+}
+
+bignum* exact_integer_sqrt(bignum * s) {
+  auto res = bignum_alloc(1);
+  bignum_set_from_fixnum(res, 1);
+  if (bignum_cmp(s, res) != 1) {
+    return s;
+  }
+  bignum_set_from_fixnum(res, 2);
+  auto x0 = bignum_div(s, res);
+  auto x1 = bignum_div( bignum_add_unsigned(x0, bignum_div(s, x0)), res);
+  while(bignum_cmp(x1, x0) == -1) {
+    x0 = x1;
+    x1 = bignum_div(bignum_add_unsigned(x0, (bignum_div(s, x0))), res);
+  }
+  return x0;
+}
+
+bignum* square(bignum* a) {
+  return bignum_mul_unsigned(a, a);
+}
+
+void gen_pi(uint64_t nb_digits) {
+  
+}
+
+int main(int argc, char* argv[]) {
+  int nb_digits = atoi(argv[1]);
+  auto one = bignum_alloc(1);
+  bignum_set_from_fixnum(one, 10);
+  one = expt(one, nb_digits);
+
+  auto two = bignum_alloc(1);
+  bignum_set_from_fixnum(two, 2);
+
+  auto four = bignum_alloc(1);
+  bignum_set_from_fixnum(four, 4);
+
+  auto oneone = bignum_alloc(1);
+  bignum_set_from_fixnum(oneone, 1);
+
+  auto a = one;
+  auto b = exact_integer_sqrt(bignum_div(square(one), two));
+  auto t = bignum_div(one, four);
+  auto x = oneone;
+  bignum* res;
+  while (true) {
   {
     char buf1[1024];
     bignum_print_hex(buf1, 1024, a);
@@ -341,25 +430,44 @@ int main() {
   }
   {
     char buf1[1024];
-    bignum_print_hex(buf1, 1024, d);
-    printf("D: %s\n", buf1);
+    bignum_print_hex(buf1, 1024, b);
+    printf("B: %s\n", buf1);
   }
-  assert(bignum_cmp(a, d) == 1);
-  a = bignum_div(a, d);
-  char buf1[1024];
-  bignum_print_hex(buf1, 1024, a);
-  printf("%s\n", buf1);
-
-  free(a);
-
-  char buf2[1024];
-  gmp_printf("A: %Zx\n", am);
-  gmp_printf("D: %Zx\n", dm);
-  mpz_tdiv_q(am, am, dm);
-  gmp_snprintf(buf2, 1024, "%Zx", am);
-  printf("%s\n", buf2);
-  assert(strcmp(buf2, buf1) == 0);
-
-  mpz_clears(am, bm, dm, nullptr);
+  {
+    char buf1[1024];
+    bignum_print_hex(buf1, 1024, t);
+    printf("T: %s\n", buf1);
+  }
+  {
+    char buf1[1024];
+    bignum_print_hex(buf1, 1024, x);
+    printf("X: %s\n", buf1);
+  }
+    if (bignum_cmp(a, b) == 0) {
+      res = bignum_div(square(bignum_add_unsigned(a, b)), bignum_mul_unsigned(four, t));
+      break;
+    }
+    auto newa = bignum_div(bignum_add_unsigned(a, b), two);
+    auto diff = bignum_cmp(newa, a) == 1 ? bignum_sub_unsigned(newa, a) : bignum_sub_unsigned(a, newa);
+    auto newb = exact_integer_sqrt(bignum_mul_unsigned(a, b));
+    auto newt = bignum_sub_unsigned(t,
+				    bignum_div(
+					       bignum_mul_unsigned(x,
+								   square(
+									  diff)),
+					       one));
+    auto newx = bignum_mul_unsigned(two, x);
+    a = newa;
+    b = newb;
+    t = newt;
+    x = newx;
+  }
+  
+  {
+    char buf1[1024];
+    bignum_print_hex(buf1, 1024, res);
+    printf("A: %s\n", buf1);
+  }
   return 0;
 }
+
