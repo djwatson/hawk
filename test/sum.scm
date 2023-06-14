64f32d2833f3b10ac9a1cf1d167b5c00ff12df28
Dave Watson <dade.watson@gmail.com>
2023-06-14 15:32:06 -0700

    Fix debugging

--- test/sum.scm
+++ test/sum.scm
@@ -1,6 +1,7 @@
 (define (loop n sum)
-  (if (< n 0)
-      sum
-      (loop (- n 1) (+ n sum))))
+  (let loop ((n n) (sum sum))
+    (if (< n 0)
+	sum
+	(loop (- n 1) (+ n sum)))))
 
 (loop 1000000000 0)
