eb8dd687d12c9fbf106dd7d38ce31ded0a926736
Dave Watson <dade.watson@gmail.com>
2023-08-12 10:14:13 -0700

    cpstak tresting

--- test/cpstak.lua
+++ test/cpstak.lua
@@ -1,23 +1,29 @@
-function cpstak(x, y, z)
- function tak(x, y, z, k)
-    if(y >= x) then
-      return k(z)
-    else
-      
-      local c1 = function(v1)
-        local c2 = function(v2)
-	  local c3 = function(v3)
-	    return tak(v1, v2, v3, k)
+	  function c3(v3, args)
+	    return tak(args[1], args[2], v3, args[3], args[4])
 	  end
-	  return tak(z-1, x, y, c3)
+
+function c2(v2, args)
+        return tak(args[3] - 1, args[1], args[2], c3, {args[4], v2, args[5], args[6]})
 	end
-        return tak(y-1, z, x, c2)
-      end
-      return tak(x-1, y, z, c1)
+
+
+function c1(v1, args)
+      return tak(args[2]-1, args[3], args[1], c2, {args[1], args[2], args[3], v1, args[4], args[5]})
     end
+
+function tak(x, y, z, kptr, kargs)
+    if(y >= x) then
+      return kptr(z, kargs)
+    else
+         return tak(x-1, y, z, c1, {x, y, z, kptr, kargs})
+     end
+      
   end
-  local c = function(a) return a end
-  return  tak(x, y, z, c)
+
+function c(a, args) return a end
+
+function cpstak(x, y, z)
+  return  tak(x, y, z, c, {})
 end
 
 print(cpstak(32, 16, 8))
