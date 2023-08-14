38a544ec0d73b8fdd965f65523a725fdf55dab43
Dave Watson <dade.watson@gmail.com>
2023-08-14 12:50:55 -0700

    mazefun

---
+++ test/bench2/mazefun.scm
@@ -0,0 +1,310 @@
+(define (this-scheme-implementation-name)
+  (string-append "boom-" "0.1"))
+(define (inexact a) (exact->inexact a))
+(define (inexact->exact a) a)
+(define (exact a) (inexact->exact a))
+(define (square n) (* n n))
+(define (jiffies-per-second)
+  1000000000 ;; returns 1 on my Bones, which is wrong. this number should work for ?many? linuxen
+  )
+;; vector-map
+;; read-line
+;; complex/rational functions
+;; define-record-type
+(define-syntax import
+  (syntax-rules ()
+    ((import stuff ...)
+     (begin) ;; do nothing
+     )))
+(define-syntax when
+  (syntax-rules ()
+    ((when a b c ...)
+     (if a (begin b c ...)))))
+(define-syntax unless
+  (syntax-rules ()
+    ((unless a b c ...)
+     (if (not a) (begin b c ...)))))
+(define (jiffies-per-second) 1)
+;;(define (current-jiffy) (with-input-from-file "/proc/uptime" read))
+;;(define (current-second) (with-input-from-file "/proc/uptime" read))
+(define (current-jiffy) 0)
+(define (current-second) 0)
+
+;;; MAZEFUN -- Constructs a maze in a purely functional way,
+;;; written by Marc Feeley.
+
+
+
+(define foldr
+  (lambda (f base lst)
+    (define foldr-aux
+      (lambda (lst)
+        (if (null? lst)
+            base
+            (f (car lst) (foldr-aux (cdr lst))))))
+
+    (foldr-aux lst)))
+
+(define foldl
+  (lambda (f base lst)
+    (define foldl-aux
+      (lambda (base lst)
+        (if (null? lst)
+            base
+            (foldl-aux (f base (car lst)) (cdr lst)))))
+
+    (foldl-aux base lst)))
+
+(define for
+  (lambda (lo hi f)
+    (define for-aux
+      (lambda (lo)
+        (if (< lo hi)
+            (cons (f lo) (for-aux (+ lo 1)))
+            '())))
+
+    (for-aux lo)))
+
+(define concat
+  (lambda (lists)
+    (foldr append '() lists)))
+
+(define list-read
+  (lambda (lst i)
+    (if (= i 0)
+        (car lst)
+        (list-read (cdr lst) (- i 1)))))
+
+(define list-write
+  (lambda (lst i val)
+    (if (= i 0)
+        (cons val (cdr lst))
+        (cons (car lst) (list-write (cdr lst) (- i 1) val)))))
+
+(define list-remove-pos
+  (lambda (lst i)
+    (if (= i 0)
+        (cdr lst)
+        (cons (car lst) (list-remove-pos (cdr lst) (- i 1))))))
+
+(define duplicates?
+  (lambda (lst)
+    (if (null? lst)
+        #f
+        (or (member (car lst) (cdr lst))
+            (duplicates? (cdr lst))))))
+
+(define make-matrix
+  (lambda (n m init)
+    (for 0 n (lambda (i) (for 0 m (lambda (j) (init i j)))))))
+
+(define matrix-read
+  (lambda (mat i j)
+    (list-read (list-read mat i) j)))
+
+(define matrix-write
+  (lambda (mat i j val)
+    (list-write mat i (list-write (list-read mat i) j val))))
+
+(define matrix-size
+  (lambda (mat)
+    (cons (length mat) (length (car mat)))))
+
+(define matrix-map
+  (lambda (f mat)
+    (map (lambda (lst) (map f lst)) mat)))
+
+(define initial-random 0)
+
+(define next-random-number
+  (lambda (current-random)
+    (remainder (+ (* current-random 3581) 12751) 131072)))
+
+(define shuffle
+  (lambda (lst)
+    (shuffle-aux lst initial-random)))
+
+(define shuffle-aux
+  (lambda (lst current-random)
+    (if (null? lst)
+        '()
+        (let ((new-random (next-random-number current-random)))
+          (let ((i (remainder new-random (length lst))))
+            (cons (list-read lst i)
+                  (shuffle-aux (list-remove-pos lst i)
+                               new-random)))))))
+
+(define make-maze
+  (lambda (n m) ; n and m must be odd
+    (if (not (and (odd? n) (odd? m)))
+        'error
+        (let ((cave
+               (make-matrix n m (lambda (i j)
+                                  (if (and (even? i) (even? j))
+                                      (cons i j)
+                                      #f))))
+              (possible-holes
+               (concat
+                (for 0 n (lambda (i)
+                           (concat
+                            (for 0 m (lambda (j)
+                                       (if (equal? (even? i) (even? j))
+                                           '()
+                                           (list (cons i j)))))))))))
+          (cave-to-maze (pierce-randomly (shuffle possible-holes) cave))))))
+
+(define cave-to-maze
+  (lambda (cave)
+    (matrix-map (lambda (x) (if x '_ '*)) cave)))
+
+(define pierce
+  (lambda (pos cave)
+    (let ((i (car pos)) (j (cdr pos)))
+      (matrix-write cave i j pos))))
+
+(define pierce-randomly
+  (lambda (possible-holes cave)
+    (if (null? possible-holes)
+        cave
+        (let ((hole (car possible-holes)))
+          (pierce-randomly (cdr possible-holes)
+                           (try-to-pierce hole cave))))))
+
+(define try-to-pierce
+  (lambda (pos cave)
+    (let ((i (car pos)) (j (cdr pos)))
+      (let ((ncs (neighboring-cavities pos cave)))
+        (if (duplicates?
+             (map (lambda (nc) (matrix-read cave (car nc) (cdr nc))) ncs))
+            cave
+            (pierce pos
+                    (foldl (lambda (c nc) (change-cavity c nc pos))
+                           cave
+                           ncs)))))))
+
+(define change-cavity
+  (lambda (cave pos new-cavity-id)
+    (let ((i (car pos)) (j (cdr pos)))
+      (change-cavity-aux cave pos new-cavity-id (matrix-read cave i j)))))
+
+(define change-cavity-aux
+  (lambda (cave pos new-cavity-id old-cavity-id)
+    (let ((i (car pos)) (j (cdr pos)))
+      (let ((cavity-id (matrix-read cave i j)))
+        (if (equal? cavity-id old-cavity-id)
+            (foldl (lambda (c nc)
+                     (change-cavity-aux c nc new-cavity-id old-cavity-id))
+                   (matrix-write cave i j new-cavity-id)
+                   (neighboring-cavities pos cave))
+            cave)))))
+
+(define neighboring-cavities
+  (lambda (pos cave)
+    (let ((size (matrix-size cave)))
+      (let ((n (car size)) (m (cdr size)))
+        (let ((i (car pos)) (j (cdr pos)))
+          (append (if (and (> i 0) (matrix-read cave (- i 1) j))
+                      (list (cons (- i 1) j))
+                      '())
+                  (if (and (< i (- n 1)) (matrix-read cave (+ i 1) j))
+                      (list (cons (+ i 1) j))
+                      '())
+                  (if (and (> j 0) (matrix-read cave i (- j 1)))
+                      (list (cons i (- j 1)))
+                      '())
+                  (if (and (< j (- m 1)) (matrix-read cave i (+ j 1)))
+                      (list (cons i (+ j 1)))
+                      '())))))))
+
+(define (main)
+  (let* ((count 10000)
+         (input1 11)
+         (input2 11)
+         (output '((_ * _ _ _ _ _ _ _ _ _)
+ (_ * * * * * * * _ * *)
+ (_ _ _ * _ _ _ * _ _ _)
+ (_ * _ * _ * _ * _ * _)
+ (_ * _ _ _ * _ * _ * _)
+ (* * _ * * * * * _ * _)
+ (_ * _ _ _ _ _ _ _ * _)
+ (_ * _ * _ * * * * * *)
+ (_ _ _ * _ _ _ _ _ _ _)
+ (_ * * * * * * * _ * *)
+ (_ * _ _ _ _ _ _ _ _ _)))
+         (s3 (number->string count))
+         (s2 (number->string input2))
+         (s1 (number->string input1))
+         (name "mazefun"))
+    (run-r7rs-benchmark
+     (string-append name ":" s1 ":" s2 ":" s3)
+     count
+     (lambda () (make-maze (hide count input1) (hide count input2)))
+     (lambda (result) (equal? result output)))))
+
+;;; The following code is appended to all benchmarks.
+
+;;; Given an integer and an object, returns the object
+;;; without making it too easy for compilers to tell
+;;; the object will be returned.
+
+(define (hide r x)
+  (call-with-values
+   (lambda ()
+     (values (vector values (lambda (x) x))
+             (if (< r 100) 0 1)))
+   (lambda (v i)
+     ((vector-ref v i) x))))
+
+;;; Given the name of a benchmark,
+;;; the number of times it should be executed,
+;;; a thunk that runs the benchmark once,
+;;; and a unary predicate that is true of the
+;;; correct results the thunk may return,
+;;; runs the benchmark for the number of specified iterations.
+
+(define (run-r7rs-benchmark name count thunk ok?)
+
+  ;; Rounds to thousandths.
+  (define (rounded x)
+    (/ (round (* 1000 x)) 1000))
+
+  (display "Running ")
+  (display name)
+  (newline)
+  (flush-output-port (current-output-port))
+  (let* ((j/s (jiffies-per-second))
+         (t0 (current-second))
+         (j0 (current-jiffy)))
+    (let loop ((i 0)
+               (result #f))
+      (cond ((< i count)
+             (loop (+ i 1) (thunk)))
+            ((ok? result)
+             (let* ((j1 (current-jiffy))
+                    (t1 (current-second))
+                    (jifs (- j1 j0))
+                    (secs (inexact (/ jifs j/s)))
+                    (secs2 (rounded (- t1 t0))))
+               (display "Elapsed time: ")
+               (write secs)
+               (display " seconds (")
+               (write secs2)
+               (display ") for ")
+               (display name)
+               (newline)
+               (display "+!CSVLINE!+")
+               (display (this-scheme-implementation-name))
+               (display ",")
+               (display name)
+               (display ",")
+               (display secs)
+               (newline)
+               (flush-output-port (current-output-port)))
+             result)
+            (else
+             (display "ERROR: returned incorrect result: ")
+             (write result)
+             (newline)
+             (flush-output-port (current-output-port))
+             result)))))
+(main)
