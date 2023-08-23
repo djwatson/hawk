37aa301560c40782b79b33b2a4d2d1e7cf2692f4
Dave Watson <dade.watson@gmail.com>
2023-08-23 07:56:41 -0700

    GUARD

--- record.c
+++ record.c
@@ -775,6 +775,26 @@ int record_instr(unsigned int *pc, long *frame, long argcnt) {
     regs[INS_A(i)] = push_ir(trace, op, op1, IR_NONE, type | IR_INS_TYPE_GUARD);
     break;
   }
+  case GUARD: {
+    uint32_t op1 = record_stack_load(INS_B(i), frame);
+    int64_t v = frame[INS_B(i)];
+    auto type = INS_C(i);
+    int64_t c = FALSE_REP;
+    if ((type & TAG_MASK) == LITERAL_TAG) {
+      if (type == (v & IMMEDIATE_MASK)) {
+	c = TRUE_REP;
+      }
+    } else {
+      if (type == (v & TAG_MASK)) {
+	c = TRUE_REP;
+      }
+    }
+    // TODO ptr
+    auto knum = arrlen(trace->consts);
+    arrput(trace->consts, c);
+    regs[INS_A(i)] = IR_CONST_BIAS + knum;
+    break;
+  }
   case JNGUARD: 
   case JGUARD: {
     record_stack_load(INS_B(i), frame);
