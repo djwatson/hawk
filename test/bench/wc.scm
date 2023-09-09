decf01a5a5292acf63449da1b30d1d683ca9ad31
Dave Watson <dade.watson@gmail.com>
2023-09-09 14:44:40 -0700

    Move tests around

--- test/bench/wc.scm
+++ test/bench/wc.scm
@@ -26,7 +26,7 @@
           (wcport port)))))
 
 (define (go)
-  (set! inport (open-input-file "./test/bench/bib"))
+  (set! inport (open-input-file "bench/bib"))
   (set! nl 0)
   (set! nw 0)
   (set! nc 0)
