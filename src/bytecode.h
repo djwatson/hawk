6e0d42b5034533f5ed3fd51378da0287177de5e0
Dave Watson <dade.watson@gmail.com>
2023-10-12 10:21:05 -0700

    lint: bytecode.h

--- src/bytecode.h
+++ src/bytecode.h
@@ -1,18 +1,22 @@
+// Copyright 2023 Dave Watson
+
 #pragma once
 
 #include <stdint.h>
 
+#include "types.h"
+
 extern const char *ins_names[];
-extern long *const_table;
-extern unsigned long const_table_sz;
+extern gc_obj *const_table;
+extern uint64_t const_table_sz;
 
 #define CODE(i, a, b, c) (((c) << 24) | ((b) << 16) | ((a) << 8) | (i))
 #define CODE_D(i, a, d) (((d) << 16) | ((a) << 8) | (i))
 static inline uint8_t INS_OP(uint32_t i) { return i & 0xff; }
-#define INS_A(i) ((i >> 8) & 0xff)
-#define INS_B(i) ((i >> 16) & 0xff)
-#define INS_C(i) ((i >> 24) & 0xff)
-#define INS_D(i) (i >> 16)
+#define INS_A(i) (((i) >> 8) & 0xff)
+#define INS_B(i) (((i) >> 16) & 0xff)
+#define INS_C(i) (((i) >> 24) & 0xff)
+#define INS_D(i) ((i) >> 16)
 
 typedef struct bcfunc {
   char *name;
--- src/defs.h
+++ src/defs.h
@@ -1,3 +1,5 @@
+// Copyright 2023 Dave Watson
+
 #pragma once
 
 #define EXPORT __attribute__((visibility("default")))
