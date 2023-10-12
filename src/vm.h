4ab67ea63afbaa7cdcd857a4a3880cc1c412d18d
Dave Watson <dade.watson@gmail.com>
2023-10-12 10:23:03 -0700

    lint: vm.h

--- src/vm.h
+++ src/vm.h
@@ -1,11 +1,12 @@
+// Copyright 2023 Dave Watson
+
 #pragma once
 
 #include <stdint.h>
 
-#include "defs.h"
 #include "types.h"
 
-void run(bcfunc *func, long argcnt, const long *args);
+void run(bcfunc *func, int64_t argcnt, const gc_obj *args);
 bcfunc *find_func_for_frame(const uint32_t *pc);
 void free_vm();
 
@@ -18,16 +19,16 @@ extern bcfunc **funcs;
 #define hotmap_tail_rec 1
 #define hotmap_mask (hotmap_sz - 1)
 
-long vm_read_char(port_s *port);
-long vm_peek_char(port_s *port);
-long vm_string_symbol(string_s *str);
-void vm_write(long obj, long port);
-void vm_make_string(long str, long ch);
-void vm_make_vector(long vec, long v);
-long vm_callcc(long *frame);
-long vm_cc_resume(long c);
-void expand_stack(long **o_frame);
-long vm_length(long fb);
-long vm_memq(long fb, long fc);
-long vm_assq(long fb, long fc);
-long vm_assv(long fb, long fc);
+gc_obj vm_read_char(port_s *port);
+gc_obj vm_peek_char(port_s *port);
+gc_obj vm_string_symbol(string_s *str);
+void vm_write(gc_obj obj, gc_obj port);
+void vm_make_string(gc_obj str, gc_obj ch);
+void vm_make_vector(gc_obj vec, gc_obj v);
+gc_obj vm_callcc(gc_obj *frame);
+gc_obj vm_cc_resume(gc_obj c);
+void expand_stack(gc_obj **o_frame);
+gc_obj vm_length(gc_obj fb);
+gc_obj vm_memq(gc_obj fb, gc_obj fc);
+gc_obj vm_assq(gc_obj fb, gc_obj fc);
+gc_obj vm_assv(gc_obj fb, gc_obj fc);
