fe4ba8d815f5aa9127c618f9b384f25babb0e065
Dave Watson <dade.watson@gmail.com>
2023-10-12 10:23:51 -0700

    lint record.h

--- src/record.h
+++ src/record.h
@@ -1,11 +1,14 @@
+// Copyright 2023 Dave Watson
+
 #pragma once
 
-#include "ir.h"
 #include <stdbool.h>
 #include <stdint.h>
 
-int record(unsigned int *pc, long *frame, long argcnt);
-int record_instr(unsigned int *pc, long *frame, long argcnt);
+#include "ir.h"
+
+int record(uint32_t *pc, gc_obj *frame, int64_t argcnt);
+int record_instr(uint32_t *pc, gc_obj *frame, int64_t argcnt);
 void trace_flush(trace_s *ctrace, bool all);
 void record_side(trace_s *parent, snap_s *side);
 trace_s *trace_cache_get(uint16_t tnum);
