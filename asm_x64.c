2a96a243f22902b73173208e5c0219e0bb7a564b
Dave Watson <dade.watson@gmail.com>
2025-11-11 12:34:59 -0800

    asm_x64 gemini stuff

--- asm_x64.c
+++ asm_x64.c
@@ -10,13 +10,14 @@
 #include "asm.h"
 #include "hawk.h"
 
+static void emit_reg_reg(emit_state *s, uint8_t opcode, uint8_t src,
+                         uint8_t dst);
+
 const char *const reg_names[MAX_REG] = {
 #define X(name) #name,
     ASM_X64_REGISTER_LIST(X)
 #undef X
 };
-
-void emit_reg_reg(emit_state *s, uint8_t opcode, uint8_t src, uint8_t dst);
 
 void asm_mark_unallocatable(bool used[MAX_REG]) {
   used[RSP] = true;
@@ -29,26 +30,28 @@ static uint8_t low3bits(uint8_t r) { return 0x7 & r; }
 /////////////////// instruction encoding
 
 #define p (s->p)
-void emit_rex(emit_state *s, uint8_t w, uint8_t r, uint8_t x, uint8_t b) {
+static void emit_rex(emit_state *s, uint8_t w, uint8_t r, uint8_t x,
+                     uint8_t b) {
   *(--p) = 0x40 | (w << 3) | (r << 2) | (x << 1) | b;
 }
 
-void emit_modrm(emit_state *s, uint8_t mod, uint8_t reg, uint8_t rm) {
+static void emit_modrm(emit_state *s, uint8_t mod, uint8_t reg, uint8_t rm) {
   *(--p) = (mod << 6) | (reg << 3) | rm;
 }
 
-void emit_sib(emit_state *s, uint8_t scale, uint8_t index, uint8_t base) {
+static void emit_sib(emit_state *s, uint8_t scale, uint8_t index,
+                     uint8_t base) {
   *(--p) = (scale << 6) | ((0x7 & index) << 3) | ((0x7 & base));
 }
 
-void emit_imm8(emit_state *s, uint8_t imm) { *(--p) = imm; }
+static void emit_imm8(emit_state *s, uint8_t imm) { *(--p) = imm; }
 
-void emit_imm64(emit_state *s, int64_t imm) {
+static void emit_imm64(emit_state *s, int64_t imm) {
   p -= sizeof(int64_t);
   memcpy(p, &imm, sizeof(imm));
 }
 
-void emit_imm32(emit_state *s, int32_t imm) {
+static void emit_imm32(emit_state *s, int32_t imm) {
   p -= sizeof(int32_t);
   memcpy(p, &imm, sizeof(imm));
 }
@@ -81,20 +84,20 @@ void emit_mov64(emit_state *s, uint8_t r, int64_t imm) {
 #endif
 }
 
-void emit_call_indirect(emit_state *s, uint8_t r) {
+static void emit_call_indirect(emit_state *s, uint8_t r) {
   emit_modrm(s, 0x3, 0x2, 0x7 & r);
   *(--p) = 0xff;
   emit_rex(s, 1, 0, 0, r >> 3);
 }
 
-void emit_call_indirect_mem(emit_state *s, int32_t offset) {
+static void emit_call_indirect_mem(emit_state *s, int32_t offset) {
   emit_imm32(s, offset);
   emit_modrm(s, 0x00, 2, RBP);
   *(--p) = 0xff;
   emit_rex(s, 1, 0, 0, 0);
 }
 
-void emit_call32(emit_state *s, int32_t offset) {
+static void emit_call32(emit_state *s, int32_t offset) {
   emit_imm32(s, offset);
   *(--p) = 0xe8;
 }
@@ -103,8 +106,8 @@ void emit_ret(emit_state *s) { *(--p) = 0xc3; }
 
 // TODO(djwatson) clean this up.  THe main issue is REX needs W=0.
 // Also check R1 does full checks for rsp/rbp
-void emit_cmp_mem32_imm32(emit_state *s, int32_t offset, uint8_t r1,
-                          int32_t imm) {
+static void emit_cmp_mem32_imm32(emit_state *s, int32_t offset, uint8_t r1,
+                                 int32_t imm) {
   emit_imm32(s, imm);
   assert(r1 != RSP);
   assert(r1 != RBP);
@@ -123,7 +126,7 @@ void emit_cmp_mem32_imm32(emit_state *s, int32_t offset, uint8_t r1,
   *(--p) = 0x81;
   emit_rex(s, 0, 0, 0, r1 >> 3);
 }
-void emit_cmp_reg_imm32(emit_state *s, uint8_t r, int32_t imm) {
+static void emit_cmp_reg_imm32(emit_state *s, uint8_t r, int32_t imm) {
   if ((int32_t)((int8_t)imm) == imm) {
     *(--p) = imm;
     emit_reg_reg(s, 0x83, 7, r);
@@ -133,7 +136,7 @@ void emit_cmp_reg_imm32(emit_state *s, uint8_t r, int32_t imm) {
   }
 }
 
-void emit_cmp_reg_reg(emit_state *s, uint8_t src, uint8_t dst) {
+static void emit_cmp_reg_reg(emit_state *s, uint8_t src, uint8_t dst) {
   emit_modrm(s, 0x3, 0x7 & src, 0x7 & dst);
   *(--p) = 0x3b;
   emit_rex(s, 1, src >> 3, 0, dst >> 3);
@@ -159,13 +162,13 @@ void emit_jmp32(emit_state *s, int64_t target) {
   *(--p) = 0xe9;
 }
 
-void emit_jmp_indirect(emit_state *s, int32_t offset) {
+static void emit_jmp_indirect(emit_state *s, int32_t offset) {
   emit_imm32(s, offset);
   emit_modrm(s, 0x0, 4, RBP);
   *(--p) = 0xff;
 }
 
-void emit_jmp_abs(emit_state *s, enum registers r) {
+static void emit_jmp_abs(emit_state *s, enum registers r) {
   emit_modrm(s, 0x3, 4, 0x7 & r);
   *(--p) = 0xff;
   if (r >> 3) {
@@ -173,21 +176,24 @@ void emit_jmp_abs(emit_state *s, enum registers r) {
   }
 }
 
-void emit_reg_reg(emit_state *s, uint8_t opcode, uint8_t src, uint8_t dst) {
+static void emit_reg_reg(emit_state *s, uint8_t opcode, uint8_t src,
+                         uint8_t dst) {
   emit_modrm(s, 0x3, 0x7 & src, 0x7 & dst);
   *(--p) = opcode;
   emit_rex(s, 1, src >> 3, 0, dst >> 3);
 }
 
-void emit_reg_reg2(emit_state *s, uint8_t opcode, uint8_t src, uint8_t dst) {
+static void emit_reg_reg2(emit_state *s, uint8_t opcode, uint8_t src,
+                          uint8_t dst) {
   emit_modrm(s, 0x3, 0x7 & src, 0x7 & dst);
   *(--p) = opcode;
   *(--p) = 0x0f;
   emit_rex(s, 1, src >> 3, 0, dst >> 3);
 }
 
-void emit_mem_reg_sib(emit_state *s, uint8_t opcode, int32_t offset,
-                      uint8_t scale, uint8_t index, uint8_t base, uint8_t reg) {
+static void emit_mem_reg_sib(emit_state *s, uint8_t opcode, int32_t offset,
+                             uint8_t scale, uint8_t index, uint8_t base,
+                             uint8_t reg) {
   if ((int32_t)((int8_t)offset) == offset) {
     *(--p) = (int8_t)offset;
     emit_sib(s, scale, index, base);
@@ -201,9 +207,9 @@ void emit_mem_reg_sib(emit_state *s, uint8_t opcode, int32_t offset,
   emit_rex(s, 1, reg >> 3, index >> 3, base >> 3);
 }
 
-void emit_mem_reg_sib2(emit_state *s, uint8_t opcode, int32_t offset,
-                       uint8_t scale, uint8_t index, uint8_t base,
-                       uint8_t reg) {
+static void emit_mem_reg_sib2(emit_state *s, uint8_t opcode, int32_t offset,
+                              uint8_t scale, uint8_t index, uint8_t base,
+                              uint8_t reg) {
   if ((int32_t)((int8_t)offset) == offset) {
     *(--p) = (int8_t)offset;
     emit_sib(s, scale, index, base);
@@ -218,8 +224,8 @@ void emit_mem_reg_sib2(emit_state *s, uint8_t opcode, int32_t offset,
   emit_rex(s, 1, reg >> 3, index >> 3, base >> 3);
 }
 
-void emit_mem_reg(emit_state *s, uint8_t opcode, int32_t offset, uint8_t r1,
-                  uint8_t r2) {
+static void emit_mem_reg(emit_state *s, uint8_t opcode, int32_t offset,
+                         uint8_t r1, uint8_t r2) {
   if (low3bits(r1) == RSP) {
     emit_mem_reg_sib(s, opcode, offset, 0, RSP, r1, r2);
   } else {
@@ -243,8 +249,8 @@ void emit_mem_load(emit_state *s, int32_t offset, uint8_t base, uint8_t dst) {
 }
 
 // TODO(djwatson) merge the '2' byte versions
-void emit_mem_reg2(emit_state *s, uint8_t opcode, int32_t offset, uint8_t r1,
-                   uint8_t r2) {
+static void emit_mem_reg2(emit_state *s, uint8_t opcode, int32_t offset,
+                          uint8_t r1, uint8_t r2) {
   if (low3bits(r1) == RSP) {
     emit_mem_reg_sib2(s, opcode, offset, 0, RSP, r1, r2);
   } else {
@@ -263,14 +269,14 @@ void emit_mem_reg2(emit_state *s, uint8_t opcode, int32_t offset, uint8_t r1,
 
 /////////////////// opcodes
 
-void emit_op_imm32(emit_state *s, uint8_t opcode, uint8_t r1, uint8_t r2,
-                   int32_t imm) {
+static void emit_op_imm32(emit_state *s, uint8_t opcode, uint8_t r1, uint8_t r2,
+                          int32_t imm) {
   emit_imm32(s, imm);
   emit_reg_reg(s, opcode, r1, r2);
 }
 
-void emit_arith_imm(emit_state *s, enum ARITH_CODES op, uint8_t src,
-                    int32_t imm) {
+static void emit_arith_imm(emit_state *s, enum ARITH_CODES op, uint8_t src,
+                           int32_t imm) {
   if ((int32_t)((int8_t)imm) == imm) {
     *(--p) = imm;
     emit_reg_reg(s, 0x83, op, src);
@@ -287,23 +293,21 @@ static void emit_neg(emit_state *s, uint8_t r) {
   }
 }
 
-void emit_push(emit_state *s, uint8_t r) {
+static void emit_push(emit_state *s, uint8_t r) {
   *(--p) = 0x50 + (0x7 & r);
   if (r >> 3) {
     emit_rex(s, 0, 0, 0, r >> 3);
   }
 }
 
-void emit_pop(emit_state *s, uint8_t r) {
-  // emit_modrm(s, 0x3, 0, 0x7 & r);
-  //*(--p) = 0x8f;
+static void emit_pop(emit_state *s, uint8_t r) {
   *(--p) = 0x58 | (0x7 & r);
   if (r >> 3) {
     emit_rex(s, 0, 0, 0, r >> 3);
   }
 }
 
-void emit_cmovl(emit_state *s, uint8_t dst, uint8_t src) {
+static void emit_cmovl(emit_state *s, uint8_t dst, uint8_t src) {
   emit_modrm(s, 0x3, 0x7 & src, 0x7 & dst);
   *(--p) = 0x4c;
   *(--p) = 0x0f;
