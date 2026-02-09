c325817c5d5bed08291969255f50d70a85a12989
Dave Watson <dade.watson@gmail.com>
2026-02-09 09:37:49 -0800

    Add primes.scm to tests

--- test/cpstak.scm
+++ test/cpstak.scm
@@ -1,6 +1,6 @@
 ;;; CPSTAK -- A continuation-passing version of the TAK benchmark.
 ;;; A good test of first class procedures and tail recursion.
-;(import (scheme r5rs))
+(import (scheme r5rs))
 
 (define (tak x y z k)
   (if (>= y x)
