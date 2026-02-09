721629c518b85df72ca04f53c7e555e91f854023
Dave Watson <dade.watson@gmail.com>
2026-02-09 09:06:08 -0800

    add primes.scm

---
+++ test/primes.scm
@@ -0,0 +1,43 @@
+;;; PRIMES -- Compute primes less than 100, written by Eric Mohr.
+
+;;; LC NOTE : Can't compute more primes because of heap/stack overflow
+(import (except (scheme base) pair? cdr cons length null? car cddr append not) (scheme write)
+        (prefix (hawk sys) sys:))
+
+(define (cons a b)
+  (let ((cell (sys:ALLOC 24 3))) (sys:STORE cell a 0) (sys:STORE cell b 1) cell))
+(define (not a) (if a #f #t))
+(define (null? a) (sys:GUARD a 20))
+(define (pair? a) (sys:GUARD a 3))
+(define (cdr a) (sys:LOAD a 1))
+(define (car a) (sys:LOAD a 0))
+
+(define (interval-list m n)
+  (if (> m n) '() (cons m (interval-list (+ 1 m) n))))
+
+(define (remove-multiples n l)
+  (if (null? l)
+      '()
+      (if (= (modulo (car l) n) 0)
+          (remove-multiples n (cdr l))
+          (cons (car l) (remove-multiples n (cdr l))))))
+
+(define (sieve l)
+  (if (null? l) '() (cons (car l) (sieve (remove-multiples (car l) (cdr l))))))
+
+(define (primes<= n) (sieve (interval-list 2 n)))
+
+(define (pp x) (display x) (display "\n"))
+;; (pp (primes<= 0))
+;; (pp (primes<= 1))
+;; (pp (primes<= 50))
+;; (pp (primes<= 80))
+(define (run-loop n res) (if (= n 0) res (run-loop (- n 1) (primes<= 1000))))
+(pp (run-loop 10000 0))
+
+;()
+;()
+;(2 3 5 7 11 13 17 19 23 29 31 37 41 43 47)
+;(2 3 5 7 11 13 17 19 23 29 31 37 41 43 47 53 59 61 67 71 73 79)
+
+
